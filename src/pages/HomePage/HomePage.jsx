4caefc69b27cd4a2cf3c1e4bf6344e3117f68371
kiki luo <kikiluo0411@gmail.com>
2024-08-11 13:51:58 +1000

    Complete gift season on homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -25,12 +25,13 @@ export default function HomePage() {
           </div>
         </div>
       </div>
+
       {/* shop by category */}
-      <h1>Shop By Category</h1>
-      <div class="row row-cols-1 row-cols-md-5 g-4">
+      <h3>Shop By Category</h3>
+      <div className="row row-cols-1 row-cols-md-5 g-4">
         <div class="col">
           <div class="card h-100">
-            <Link to="/jewellery/newrelease">
+            <Link className="nav-link" to="/jewellery/newrelease">
               <img
                 src="https://kokoandco.s3.ap-southeast-2.amazonaws.com/necklace/Butterfly+vintage+2.jpg"
                 class="card-img-top"
@@ -44,7 +45,7 @@ export default function HomePage() {
         </div>
         <div class="col">
           <div class="card h-100">
-            <Link to="/jewellery/bracelets">
+            <Link className="nav-link" to="/jewellery/bracelets">
               <img
                 src="https://kokoandco.s3.ap-southeast-2.amazonaws.com/bracelets/Love+links+2.png"
                 class="card-img-top"
@@ -58,7 +59,7 @@ export default function HomePage() {
         </div>
         <div class="col">
           <div class="card h-100">
-            <Link to="/jewellery/earrings">
+            <Link className="nav-link" to="/jewellery/earrings">
               <img
                 src="https://kokoandco.s3.ap-southeast-2.amazonaws.com/earrings/Curly+C+2.png"
                 class="card-img-top"
@@ -72,7 +73,7 @@ export default function HomePage() {
         </div>
         <div class="col">
           <div class="card h-100">
-            <Link to="/jewellery/necklaces">
+            <Link className="nav-link" to="/jewellery/necklaces">
               <img
                 src="https://kokoandco.s3.ap-southeast-2.amazonaws.com/necklace/Rose+avenue+1.jpg"
                 class="card-img-top"
@@ -86,7 +87,7 @@ export default function HomePage() {
         </div>
         <div class="col">
           <div class="card h-100">
-            <Link to="/jewellery/rings">
+            <Link className="nav-link" to="/jewellery/rings">
               <img
                 src="https://kokoandco.s3.ap-southeast-2.amazonaws.com/rings/miss+u+2.jpg"
                 class="card-img-top"
@@ -99,7 +100,92 @@ export default function HomePage() {
           </div>
         </div>
       </div>
+
       {/* gift season */}
+      <div className="card mb-3 carousel">
+      <div className="row g-0">
+        <div className="col-md-8">
+          <div
+            id="carouselExampleIndicators"
+            className="carousel slide"
+            data-bs-ride="carousel"
+          >
+            <div className="carousel-indicators">
+              <button
+                type="button"
+                data-bs-target="#carouselExampleIndicators"
+                data-bs-slide-to="0"
+                className="active"
+                aria-current="true"
+                aria-label="Slide 1"
+              ></button>
+              <button
+                type="button"
+                data-bs-target="#carouselExampleIndicators"
+                data-bs-slide-to="1"
+                aria-label="Slide 2"
+              ></button>
+            </div>
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img
+                  src="https://kokoandco.s3.ap-southeast-2.amazonaws.com/Home+page/gitf+season1.jpg"
+                  className="d-block w-100"
+                  alt="giftseason"
+                ></img>
+              </div>
+              <div className="carousel-item">
+                <img
+                  src="https://kokoandco.s3.ap-southeast-2.amazonaws.com/Home+page/gitf+season2.jpg"
+                  className="d-block w-100"
+                  alt="giftseason"
+                ></img>
+              </div>
+
+              <button
+                className="carousel-control-prev"
+                type="button"
+                data-bs-target="#carouselExampleIndicators"
+                data-bs-slide="prev"
+              >
+                <span
+                  className="carousel-control-prev-icon"
+                  aria-hidden="true"
+                ></span>
+                <span className="visually-hidden">Previous</span>
+              </button>
+              <button
+                className="carousel-control-next"
+                type="button"
+                data-bs-target="#carouselExampleIndicators"
+                data-bs-slide="next"
+              >
+                <span
+                  className="carousel-control-next-icon"
+                  aria-hidden="true"
+                ></span>
+                <span className="visually-hidden">Next</span>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card-body">
+            <h3 className="card-title">Perfect Spring Gifts</h3>
+            <p className="card-text">
+              Embrace the freshness of spring with our stunning jewelry
+              collection, designed to celebrate the season of renewal and
+              growth. Each piece captures the vibrant colors and delicate beauty
+              of spring, making them the perfect gift for any occasion. Whether
+              you're honoring a loved one or treating yourself, our jewelry adds
+              a touch of elegance and joy to this blossoming season. Discover
+              the perfect springtime gift that sparkles with the promise of new
+              beginnings.
+            </p>
+          </div>
+        </div>
+      </div>
+      </div>
     </>
   );
 }
